039cb31342a610ad525b840e7f8b792a133a7792
Amlana-P <amlana.pattnayak@gmail.com>
2020-10-08 08:53:40 +0530

    Added Cursur Effect

--- src/index.js
+++ src/index.js
@@ -22,3 +22,29 @@ open.addEventListener("click", function(){
         changeIcon = true;
     }
 });
+
+// Declaring necessary variables to the cursor class and navlinks
+let mouseCursor = document.querySelector('.cursor');
+let navLinks = document.querySelectorAll('.navlinks li');
+
+// Event listener
+window.addEventListener('mousemove', cursor);
+
+// cursor function
+// pageX and pageY give the realtime mouse coordinates on screen 
+function cursor(e) {
+    mouseCursor.style.top = e.pageY + 'px'
+    mouseCursor.style.left = e.pageX + 'px'
+}
+
+Array.from(navLinks).forEach(link => {
+    link.addEventListener('mouseleave', () => {
+        mouseCursor.classList.remove('link-grow');
+        link.classList.remove('hovered-link');
+    })
+
+    link.addEventListener('mouseover', () => {
+        mouseCursor.classList.add('link-grow');
+        link.classList.add('hovered-link');
+    })
+});
